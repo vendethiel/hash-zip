23fd4b06569b36f564d8609056113e03a33d29c8
vendethiel <vendethiel@hotmail.fr>
2023-07-07 19:17:11 +0200

    Add `zip_compact` method

--- lib/hash-zip.rb
+++ lib/hash-zip.rb
@@ -12,13 +12,41 @@ class Hash
     output_keys = sources.flat_map(&:keys).uniq
     if block_given?
       output_keys.each do |key|
-        yield(key, *sources.map{|source| source[key]})
+        values = sources.map{|source| source[key]}
+        yield(key, *values)
       end
       nil
     else
       result = {}
       output_keys.each do |key|
-        result[key] = sources.map{|source| source[key]}
+        values = sources.map{|source| source[key]}
+        result[key] = values
+      end
+      result
+    end
+  end
+
+  def zip_compact(*args)
+    sources = [self]
+    args.each do |arg|
+      if arg.is_a?(Hash)
+        sources << arg
+      else
+        sources << arg.to_h
+      end
+    end
+    output_keys = sources.flat_map(&:keys).uniq
+    if block_given?
+      output_keys.each do |key|
+        values = sources.map{|source| source[key]}.compact
+        yield(key, *values)
+      end
+      nil
+    else
+      result = {}
+      output_keys.each do |key|
+        values = sources.map{|source| source[key]}.compact
+        result[key] = values
       end
       result
     end
